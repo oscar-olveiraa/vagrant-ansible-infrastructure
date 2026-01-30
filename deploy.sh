1aa79f942a55174a52d574586bcf16a75a6b7e53
okinhaa ;) <91733496+oscar-olveiraa@users.noreply.github.com>
2026-01-30 22:16:45 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,6 @@ echo "======================================"
 echo " Despliegue automatico de infraestructura"
 echo "======================================"
 
-# Comprobaciones
-command -v vagrant >/dev/null 2>&1 || {
-  echo "Vagrant no esta instalado"
-  exit 1
-}
-
-#command -v virtualbox >/dev/null 2>&1 || {
-#  echo "VirtualBox no esta instalado"
-#  exit 1
-#}
-
-echo "[+] Dependencias OK"
-
 # Levantar infraestructura
 echo "[+] Levantando VM..."
 vagrant up --provision
